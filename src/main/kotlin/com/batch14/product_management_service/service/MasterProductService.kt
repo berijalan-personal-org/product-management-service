136f798807bbb80e8ed62351f60b08304aab9fac
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2025-06-13 00:47:07 +0700

    Update MasterProductService.kt

--- src/main/kotlin/com/batch14/product_management_service/service/MasterProductService.kt
+++ src/main/kotlin/com/batch14/product_management_service/service/MasterProductService.kt
@@ -1,8 +1,9 @@
 package com.batch14.product_management_service.service
 
+import com.batch14.product_management_service.domain.dto.response.ResProductDto
 import com.batch14.product_management_service.domain.entity.MasterProductEntity
 
 interface MasterProductService {
-    fun getAllProduct(): List<MasterProductEntity>
-    fun getProductById(id: Int): MasterProductEntity
+    fun getAllProduct(): List<ResProductDto>
+    fun getProductById(id: Int): ResProductDto
 }
