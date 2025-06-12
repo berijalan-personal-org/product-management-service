6739ed11947682ba5063cc5ef8106506396870f3
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2025-06-13 00:47:09 +0700

    Update MasterProductServiceImpl.kt

--- src/main/kotlin/com/batch14/product_management_service/service/impl/MasterProductServiceImpl.kt
+++ src/main/kotlin/com/batch14/product_management_service/service/impl/MasterProductServiceImpl.kt
@@ -1,4 +1,46 @@
 package com.batch14.product_management_service.service.impl
 
-class MasterProductServiceImpl {
+import com.batch14.product_management_service.domain.dto.response.ResProductDto
+import com.batch14.product_management_service.repository.MasterCategoryRepository
+import com.batch14.product_management_service.repository.MasterProductRepository
+import com.batch14.product_management_service.service.MasterProductService
+import org.springframework.stereotype.Service
+
+@Service
+class MasterProductServiceImpl (
+    private val masterProductRepository: MasterProductRepository,
+    private val masterCategoryRepository: MasterCategoryRepository
+): MasterProductService {
+    override fun getAllProduct(): List<ResProductDto> {
+        val rawData = masterProductRepository.findAll()
+        val result = mutableListOf<ResProductDto>()
+        rawData.forEach { it ->
+            result.add(
+                ResProductDto(
+                    id = it.id,
+                    name = it.name,
+                    price = it.price,
+                    stock = it.stock,
+                    categoryId = it.category?.id,
+                    categoryName = it.category?.category
+
+                )
+            )
+        }
+        return result
+    }
+
+    override fun getProductById(id: Int): ResProductDto {
+        val product = masterProductRepository.getProductById(id)
+        return product?.let {
+            ResProductDto(
+                id = it.id,
+                name = it.name,
+                price = it.price,
+                stock = it.stock,
+                categoryId = it.category?.id,
+                categoryName = it.category?.category
+            )
+        } ?: throw Exception("Product tidak ditemukan")
+    }
 }
