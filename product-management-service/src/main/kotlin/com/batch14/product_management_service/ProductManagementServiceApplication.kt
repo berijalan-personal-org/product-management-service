ed41257ce228f398aabbe51f8145c2a0277060cc
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2025-06-12 22:53:40 +0700

    Initial commit

---
+++ product-management-service/src/main/kotlin/com/batch14/product_management_service/ProductManagementServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.batch14.product_management_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductManagementServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProductManagementServiceApplication>(*args)
+}
