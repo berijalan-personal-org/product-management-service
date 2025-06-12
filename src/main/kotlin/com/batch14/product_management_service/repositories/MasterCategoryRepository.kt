618b7e7a0eef1d377422a701cde567bd0fe74b2e
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2025-06-13 00:05:56 +0700

    Delete MasterCategoryRepository.kt

--- src/main/kotlin/com/batch14/product_management_service/repositories/MasterCategoryRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.batch14.product_management_service.repositories
-
-import com.batch14.product_management_service.domain.entity.MasterCategoryEntity
-import org.springframework.data.jpa.repository.JpaRepository
-interface MasterCategoryRepository: JpaRepository<MasterCategoryEntity, Int> {
-
-}
