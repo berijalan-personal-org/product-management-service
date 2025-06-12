c135646be7bc2ba57cf06491e7cfad0165281cb2
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2025-06-13 00:14:34 +0700

    Create MasterCategoryServiceImpl.kt

---
+++ src/main/kotlin/com/batch14/product_management_service/service/impl/MasterCategoryServiceImpl.kt
@@ -0,0 +1,26 @@
+package com.batch14.product_management_service.service.impl
+
+import com.batch14.product_management_service.domain.dto.response.ResCategoryDto
+import com.batch14.product_management_service.domain.entity.MasterCategoryEntity
+import com.batch14.product_management_service.repository.MasterCategoryRepository
+import com.batch14.product_management_service.service.MasterCategoryService
+import org.springframework.stereotype.Service
+
+@Service
+class MasterCategoryServiceImpl (
+    private val masterCategoryRepository: MasterCategoryRepository
+): MasterCategoryService {
+    override fun getAllCategory(): List<ResCategoryDto> {
+        val categories = masterCategoryRepository.findAll()
+        val result = mutableListOf<ResCategoryDto>()
+        categories.forEach { category ->
+            result.add(
+                ResCategoryDto(
+                    id = category.id,
+                    name = category.name
+                )
+            )
+        }
+        return result
+    }
+}
